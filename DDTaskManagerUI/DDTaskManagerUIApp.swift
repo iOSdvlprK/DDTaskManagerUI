fe4cee03a5c4d6043ec6cf8c01a9803c8e2dac3f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-09-29 22:48:41 +0900

    Initial Commit

---
+++ DDTaskManagerUI/DDTaskManagerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDTaskManagerUIApp.swift
+//  DDTaskManagerUI
+//
+//  Created by joe on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DDTaskManagerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
