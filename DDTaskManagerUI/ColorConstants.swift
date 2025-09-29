8d983fe2f6dff45e8a3b53be85fc4cc0d32cf012
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-09-29 23:32:53 +0900

    add color assets and GettingStartedView
    - introduced multiple color assets for UI theming

---
+++ DDTaskManagerUI/ColorConstants.swift
@@ -0,0 +1,10 @@
+//
+//  ColorConstants.swift
+//  DDTaskManagerUI
+//
+//  Created by joe on 9/29/25.
+//
+
+import SwiftUI
+
+
---
+++ DDTaskManagerUI/GettingStartedView.swift
@@ -0,0 +1,47 @@
+//
+//  GettingStartedView.swift
+//  DDTaskManagerUI
+//
+//  Created by joe on 9/29/25.
+//
+
+import SwiftUI
+
+struct GettingStartedView: View {
+    var body: some View {
+        ZStack {
+            Color("lightBlue1").ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 30) {
+                Text("Manage your daily tasks")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color("darkBlue1"))
+                
+                Text("Team and Project management with solution providing app")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color("darkBlue1"))
+                
+                ZStack {
+                    Circle()
+                        .fill(Color("white"))
+                        .frame(width: 70, height: 70)
+                        .offset(x: -50)
+                        .shadow(color: .black.opacity(0.1), radius: 4, x: 5, y: 5)
+                    
+                    Text("Get Started")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color("darkBlue1"))
+                }
+                .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    GettingStartedView()
+}
