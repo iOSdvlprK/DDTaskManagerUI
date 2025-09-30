71e90d863654afaf0335634f3af36eaf2821976e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-09-30 23:14:59 +0900

    add image asset and update GettingStartedView layout
    - added a new image asset and its reference in a new Image Constants file
    - refactored GettingStartedView to include the new image and improved layout structure

--- DDTaskManagerUI/ColorConstants.swift
+++ DDTaskManagerUI/ColorConstants.swift
@@ -7,4 +7,12 @@
 
 import SwiftUI
 
-
+let blue1 = Color("blue1")
+let darkBlue1 = Color("darkBlue1")
+let gray = Color("gray")
+let lightBlue1 = Color("lightBlue1")
+let purple1 = Color("purple1")
+let purple2 = Color("purple2")
+let purple3 = Color("purple3")
+let lightPurple = Color("lightPurple")
+let white = Color("white")
---
+++ DDTaskManagerUI/Image Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Image Constants.swift
+//  DDTaskManagerUI
+//
+//  Created by joe on 9/30/25.
+//
+
+import Foundation
+
+let gettingStartedImage = "pexels-daniela-elena-tentis-118658-370799"
