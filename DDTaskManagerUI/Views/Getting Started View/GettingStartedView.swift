97326094a3736eda24b1c062100554a756a71557
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-01 22:34:22 +0900

    clean up the code
    - splitting GettingStartedView into smaller components: ImageHeader, GetStartedView, BottomOfGettingStartedView, and ManageTasksTextView
    - this improves code organization and readability

---
+++ DDTaskManagerUI/Views/Getting Started View/GettingStartedView.swift
@@ -0,0 +1,83 @@
+//
+//  GettingStartedView.swift
+//  DDTaskManagerUI
+//
+//  Created by joe on 9/29/25.
+//
+
+import SwiftUI
+
+struct ImageHeader: View {
+    let imageDim: CGFloat = 300
+    
+    var body: some View {
+        Image(gettingStartedImage)
+            .resizable()
+            .frame(width: imageDim, height: imageDim)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+            .padding()
+    }
+}
+
+struct GetStartedView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color("white"))
+                .frame(width: 70, height: 70)
+                .offset(x: -50)
+                .shadow(color: .black.opacity(0.1), radius: 4, x: 5, y: 5)
+            
+            Text("Get Started")
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(Color("darkBlue1"))
+        }
+        .padding()
+    }
+}
+
+struct GettingStartedView: View {
+    var body: some View {
+        ZStack {
+            Color("lightBlue1").ignoresSafeArea()
+            
+            VStack {
+                ImageHeader()
+                
+                Spacer()
+                
+                BottomOfGettingStartedView()
+            }
+        }
+    }
+}
+
+struct BottomOfGettingStartedView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            ManageTasksTextView()
+            
+            GetStartedView()
+        }
+        .padding()
+    }
+}
+
+struct ManageTasksTextView: View {
+    var body: some View {
+        Text("Manage your daily tasks")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(Color("darkBlue1"))
+        
+        Text("Team and Project management with solution providing app")
+            .font(.headline)
+            .fontWeight(.bold)
+            .foregroundStyle(Color("darkBlue1"))
+    }
+}
+
+#Preview {
+    GettingStartedView()
+}
