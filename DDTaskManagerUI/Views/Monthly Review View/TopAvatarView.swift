1a9d33dd26a84354eb08caacff5d1f7d3690023e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-09 22:54:57 +0900

    add animated circle effect to TopAvatarView
    - introduced a ZStack of multiple CircleView layers around the avatar image to create a dynamic, multi-ring effect
    - added the CircleView component for customizable circle rendering

--- DDTaskManagerUI/Views/Monthly Review View/TopAvatarView.swift
+++ DDTaskManagerUI/Views/Monthly Review View/TopAvatarView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct TopAvatarView: View {
+    let circleSize: CGFloat = 40
+    
     var body: some View {
         HStack {
             ThreeLinesView(color: white, unitWidth: 5, unitHeight: 25, stretch: 0.4, spacing: 6)
             
             Spacer()
             
-            CircleImageView(imageName: person3, color: white, size: 40, lineWidth: 2)
+            ZStack {
+                ForEach(0 ..< 7) { i in
+                    let factor = 2 + 1.5 * CGFloat(i)
+                    CircleView(color: lightPurple, lineWidth: 1 / factor, opacity: 5 / CGFloat(i), dim: circleSize, scaleFactor: factor)
+                }
+                
+                CircleImageView(imageName: person3, color: white, size: circleSize, lineWidth: 2)
+            }
         }
         .padding()
+    }
+}
+
+struct CircleView: View {
+    let color: Color
+    let lineWidth: CGFloat
+    let opacity: CGFloat
+    let dim: CGFloat
+    let scaleFactor: CGFloat
+    
+    var body: some View {
+        Circle()
+            .stroke(color.opacity(opacity), lineWidth: lineWidth)
+            .frame(width: dim, height: dim)
+            .scaleEffect(scaleFactor)
     }
 }
 
